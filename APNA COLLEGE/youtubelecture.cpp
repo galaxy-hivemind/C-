2f5cb7f87986bcf826e1395f3fe2fb2e345da019
Ayush Karmore <ayush_karmore_@Ayushs-MacBook-Air.local>
2025-05-13 21:25:33 +0530

    Adding for the first time

---
+++ APNA COLLEGE/youtubelecture.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 100 ;
+    char character = 'A' ;
+    float Float = 99.9 ;
+    bool Boolean = true ;
+    double Double = 83928392.839283 ; 
+
+    float absolute_value;
+    
+    cout<< "Write your absolute value here: " ;
+    cin>> absolute_value ;
+
+    cout<< "Your absolute value is: " << absolute_value <<endl;
+
+
+    cout<< a <<endl;
+    cout<< character <<endl;
+    cout<< Float <<endl;
+    cout << Boolean <<endl;
+    cout << Double <<endl;
+    return 0;
+} 
